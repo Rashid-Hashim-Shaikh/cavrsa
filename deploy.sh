56e3c44ecdd4284860356305157bcd13873e1439
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-06-03 02:59:04 +0530

    :test: Changed server-dir

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-# Build React app
-npm install
-npm run build
-
-# Deploy to Hostinger via FTP
-echo "Deploying to Hostinger..."
-curl -T dist/* -u u252975527.cavrsa:R25572983cavrsa ftp://ftp.cavrsa.com/public_html/
-echo "Deployment to Hostinger complete."
